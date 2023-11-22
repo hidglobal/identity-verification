1fa382462257c40c8d9c8dffe88a37036f33099d
Adrian Castillo <adrian.castillo@hidglobal.com>
2023-11-22 21:07:54 +0100

    Display extracted data once IDV is complete

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
+require('dotenv').config();
+const crypto = require('crypto');
 const express = require('express');
 const path = require('path');
 const ua_parser = require('ua-parser-js');
 const qr = require('qrcode');
-require('dotenv').config();
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const onboard = new Map();
+
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.json({ limit: '10mb' }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
 
 app.post('/register', (req, res) => {
   console.log('Starting registration for ' + JSON.stringify(req.body));
+  console.log('Session: ' + req.cookies.sessionID);
   const accountCode = req.body.account || '9999';
   let environment = '';
   switch (accountCode.substring(0, 1)) {
@@ -86,12 +92,13 @@ app.post('/register', (req, res) => {
             accountCode: accountCode
           },
           postbackURL: process.env.BASE_URL + '/postback',
-          redirectURL: process.env.BASE_URL + '/continue'
+          redirectURL: process.env.BASE_URL + '/continue/' + req.cookies.sessionID
         })
       })
         .then((response) => response.json())
         .then((data) => {
           console.log(`Redirecting request ${data.requestID}`)
+          onboard.set(data.requestID, {session: req.cookies.sessionID});
           let ua = ua_parser(req.headers['user-agent']);
           if (ua.device.type !== 'mobile') {
             qr.toDataURL(data.url, { errorCorrectionLevel: 'H' }, (err, url) => {
@@ -106,6 +113,13 @@ app.post('/register', (req, res) => {
 
 app.post('/postback', (req, res) => {
   console.log('Update for request ' + req.body.requestID + ' (' + req.body.requestStatus + ')');
+  if (req.body.requestStatus === 'EXPIRED') {
+    onboard.delete(req.body.requestID);
+  } else if (req.body.requestStatus === 'SUCCESS') {
+    let update = onboard.get(req.body.requestID);
+    update.transaction = req.body.transactions[req.body.transactions.length - 1];
+    onboard.set(req.body.requestID, update);
+  }
   req.body.transactions.forEach((transaction) => {
     console.log(`  Seq: ${transaction.sequenceNumber} - ${transaction.transactionStatus}`);
     console.log(`  pii: ${JSON.stringify(Object.keys(transaction.pii))}`);
@@ -113,13 +127,44 @@ app.post('/postback', (req, res) => {
   res.send('OK');
 });
 
-app.get('/continue', (req, res) => {
-  console.log('Continue after IDV');
-  console.log(req.body);
-  res.send('<html><head><link rel="stylesheet" href="/css/styles.css"></head><body><h2>Complete</h2><p>Thanks for your submission</p></body></html>')
+app.get('/continue/:session', (req, res) => {
+  console.log(`Continue after IDV for session ${req.params.session}`);
+  onboard.forEach((value, key) => {
+    if (value.session === req.params.session) {
+      console.log(`  Found request ${key}`);
+      let update = onboard.get(key);
+      if (typeof(update.transaction) !== 'undefined') {
+        let pii = update.transaction.pii;
+        res.send(`<html>
+                    <head>
+                      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                      <link rel="stylesheet" href="/css/styles.css">
+                      <title>HID Identity Verification</title>
+                    </head>
+                    <body>
+                      <div class="page page--full-width">
+                        <main class="page__content">
+                          <div class="region region--content">
+                            <section class="section section--layout-onecol">
+                              <h2>Welcome ${pii.DQL_Final_FirstName_Result.charAt(0).toUpperCase() + pii.DQL_Final_FirstName_Result.slice(1).toLowerCase()}</h2>
+                              <img width="250" height="350" src="data:image/jpeg;base64,${pii.imageSelfie}" alt="Selfie">
+                              <p>Your ${pii.DQL_Classification_DocumentName} number <strong>${pii.DQL_Final_DocumentNumber_Result}</strong> from ${pii.DQL_Classification_DocumentIssuerCountryCode} has been accepted</p>
+                            </section>
+                          </div>
+                        </main>
+                      </div>
+                    </body>
+                  </html>`);
+        onboard.delete(key);
+      } else {
+        res.send(`<html><head><link rel="stylesheet" href="/css/styles.css"></head><body><h2>Complete</h2><p>Thanks for your submission</p></body></html>`);
+      }
+    }
+  });
 });
 
 app.get('/*', (req, res) => {
+  res.cookie('sessionID', crypto.randomUUID());
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
